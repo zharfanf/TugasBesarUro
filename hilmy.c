94d28853532e91dc9f5edf9070e0b877944dc543
Faishal Zharfan <zharfanf@gmail.com>
2021-05-22 13:49:19 +0700

    Changing ordinary pointer to defined pointer

--- hilmy.c
+++ hilmy.c
@@ -8,33 +8,36 @@ void TampilkanStatus(Status *T){
 }
 
 void StartAwal(Status *T){
-	// hp(*T) = 100;
-	// happy(*T) = 50;
-	// soc(*T) = 50;
-	// duit(*T) = 50;
-	// hygenie(*T) = 50;
-	// skor(*T) = 0;
+	hp(T) = 100;
+	happy(T) = 50;
+	soc(T) = 50;
+	duit(T) = 50;
+	hygenie(T) = 50;
+	skor(T) = 0;
 
-	T->Health = 100;
-	T->Happiness = 50;
-	T->Social = 50;
-	T->Hygenie = 50;
-	T->Money = 50;
-	T->Score = 0;
+	// old pointer to Status
+	// T->Health = 100;
+	// T->Happiness = 50;
+	// T->Social = 50;
+	// T->Hygenie = 50;
+	// T->Money = 50;
+	// T->Score = 0;
 }
 
 
 void KurangHealth(Status *T, int H){
-	T->Health -= H;
+	hp(T) -= H;
+	// T->Health -= H;
 }
 
 void TambahSkor(Status *T, int S){
-	T->Score += S;
+	skor(T) += S;
+	// T->Score += S;
 }
 
 void Mandi(Status *T){
-	
-	T->Hygenie += 25;
+	hygenie(T) += 25;
+	//T->Hygenie += 25;
 
 	KurangHealth(T, 3);
 	TambahSkor(T, 3);
@@ -42,14 +45,14 @@ void Mandi(Status *T){
 	}
 
 void Makan(Status *T){
-	if(T->Money < 5){
+	if(duit(T) < 5){
 		printf("Uang Tidak Cukup\n");
 	}
 
 	else{
-		T->Money -= 5;
-		T->Hygenie -= 2;
-		T->Happiness += 1;
+		duit(T) -= 5;
+		hygenie(T) -= 2;
+		happy(T) += 1;
 	}
 	KurangHealth(T, 2);
 	TambahSkor(T, 4);
@@ -58,9 +61,9 @@ void Makan(Status *T){
 
 
 void Tidur(Status *T){
-	T->Happiness += 8;
-	T->Hygenie -= 4;
-	T->Social -= 1;
+	happy(T) += 8;
+	hygenie(T) -= 4;
+	soc(T) -= 1;
 
 	KurangHealth(T, 1);
 	TambahSkor(T, 2);
@@ -70,10 +73,10 @@ void Tidur(Status *T){
 }
 
 void Kerja(Status *T){
-	T->Money += 14;
-	T->Hygenie -= 6;
-	T->Social -= 2;
-	T->Happiness -= 5;
+	duit(T) += 14;
+	hygenie(T) -= 6;
+	soc(T) -= 2;
+	happy(T) -= 5;
 
 	KurangHealth(T, 5);
 	TambahSkor(T, 10);
@@ -84,14 +87,14 @@ void Kerja(Status *T){
 }
 
 void Kuliah(Status *T){
-	if(T->Money < 4){
+	if(duit(T) < 4){
 		printf("Uang Tidak Mencukupi, Kerja Dulu Sana!!\n");
 	}
 	else{
-	T->Money -= 4;
-	T->Hygenie -= 3;
-	T->Social -= 3;
-	T->Happiness -= 8;
+	duit(T) -= 4;
+	hygenie(T) -= 3;
+	soc(T) -= 3;
+	happy(T) -= 8;
 }
 	KurangHealth(T, 7);
 	TambahSkor(T, 11);
@@ -101,14 +104,14 @@ void Kuliah(Status *T){
 }
 
 void Nongkrong(Status *T){
-	if(T->Money < 8){
+	if(duit(T) < 8){
 		printf("Uang Tidak Mencukupi, Kerja Dulu Sana!!\n");
 	}
 	else{
-	T->Money -= 8;
-	T->Hygenie -= 3;
-	T->Social += 9;
-	T->Happiness += 7;
+	duit(T) -= 8;
+	hygenie(T) -= 3;
+	soc(T) += 9;
+	happy(T) += 7;
 }
 	KurangHealth(T, 3);
 	TambahSkor(T, 2);
@@ -118,20 +121,20 @@ void Nongkrong(Status *T){
 }
 
 void Main(Status *T){
-	if(T->Money < 1){
+	if(duit(T) < 1){
 		printf("Uang Tidak Mencukupi, Kerja Dulu Sana!!\n");
 	}
 	else{
-	// duit(*T) -= 1;
-	// hygenie(*T) -= 8;
-	// soc(*T) += 2;
-	// happy(*T) -= 5;
+	duit(T) -= 1;
+	hygenie(T) -= 8;
+	soc(T) += 2;
+	happy(T) -= 5;
 
 
-	T->Money -= 8;
-	T->Hygenie -= 3;
-	T->Social += 9;
-	T->Happiness += 7;
+	// T->Money -= 8;
+	// T->Hygenie -= 3;
+	// T->Social += 9;
+	// T->Happiness += 7;
 }
 	KurangHealth(T, 5);
 	TambahSkor(T, 6);
@@ -141,13 +144,13 @@ void Main(Status *T){
 }
 
 void NontonTV(Status *T){
-	// soc(*T) -= 2;
-	// happy(*T) += 5;
-	// hygenie(*T) -= 1;
+	soc(T) -= 2;
+	happy(T) += 5;
+	hygenie(T) -= 1;
 
-	T->Social -= 2;
-	T->Happiness += 5;
-	T->Hygenie -= 1;
+	// T->Social -= 2;
+	// T->Happiness += 5;
+	// T->Hygenie -= 1;
 
 
 	KurangHealth(T, 3);
@@ -157,11 +160,11 @@ void NontonTV(Status *T){
 }
 
 void Browsing(Status *T){
-	// soc(*T) -= 1;
-	// happy(*T) += 1;
+	soc(T) -= 1;
+	happy(T) += 1;
 
-	T->Social -= 1;
-	T->Happiness += 1;
+	// T->Social -= 1;
+	// T->Happiness += 1;
 
 	KurangHealth(T, 1);
 	TambahSkor(T, 1);
@@ -170,13 +173,13 @@ void Browsing(Status *T){
 }
 
 void Dagang(Status *T){
-	// duit(*T) += 10;
-	// soc(*T) += 7;
-	// hygenie(*T) -= 5;
+	duit(T) += 10;
+	soc(T) += 7;
+	hygenie(T) -= 5;
 
-	T->Money += 10;
-	T->Social += 7;
-	T->Hygenie -= 5;
+	// T->Money += 10;
+	// T->Social += 7;
+	// T->Hygenie -= 5;
 
 
 	KurangHealth(T, 3);
@@ -186,34 +189,34 @@ void Dagang(Status *T){
 }
 
 void BunuhDiri(Status *T){
-	// hp(*T) = 0;
-	T->Health = 0;
+	hp(T) = 0;
+	// T->Health = 0;
 
 }
 
 void BalikanNilai(Status *T){
-	if(T->Social > 100){
-		T->Social = 100;
+	if(soc(T) > 100){
+		soc(T) = 100;
 		TambahSkor(T, 1);
 	}
-	else if(T->Social < 0){
-		T->Social = 0;
+	else if(soc(T) < 0){
+		soc(T) = 0;
 		TambahSkor(T, -1);
 	}
-	if(T->Hygenie > 100){
-		T->Hygenie = 100;
+	if(hygenie(T) > 100){
+		hygenie(T) = 100;
 		TambahSkor(T, 1);
 	}
-	else if(T->Hygenie < 0){
-		T->Hygenie = 0;
+	else if(hygenie(T) < 0){
+		hygenie(T) = 0;
 		TambahSkor(T, -1);
 	}
-	if(T->Happiness > 100){
-		T->Happiness = 100;
+	if(happy(T) > 100){
+		happy(T) = 100;
 		TambahSkor(T, 1);
 	}
-	else if(T->Happiness < 0){
-		T->Happiness = 0;
+	else if(happy(T) < 0){
+		happy(T) = 0;
 		TambahSkor(T, -1);
 
 	}
